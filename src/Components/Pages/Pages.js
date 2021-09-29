c77be3d78a05c086f4faa630eb30ec3391564132
Akshat Sethi <masterakshata@gmail.com>
2021-09-29 23:03:49 +0530

    Change to custom config

--- src/Components/Pages/Pages.js
+++ src/Components/Pages/Pages.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Pages.css';
-import Card from './../Card/Card';
+import Card from '../Card/Card';
 
 export default class Pages extends React.Component {
     render(){
